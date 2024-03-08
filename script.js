8b07e6715f5c1fcc5048051e58e6f3579e3f7816
Akash Gaur <122721474+Gaur2526@users.noreply.github.com>
2024-03-08 07:04:35 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+const randomColor = function(){
+    const hex = '0123456789ABCDEF'
+    let color = '#'
+    
+    for(let i =0;i<6;i++){
+        color = color + hex[Math.floor(Math.random() * 16)]
+    }
+    return color
+};
+
+let intervalId
+const startChangingColor = function(){
+    
+    function changeBgColor(){
+
+        document.body.style.backgroundColor = randomColor();
+    }
+    if(intervalId == null){
+        intervalId = setInterval(changeBgColor,1000);
+    }
+    
+}
+const stopChangingColor = function(){
+     clearInterval(intervalId)
+     intervalId = null
+}
+
+document.querySelector("#start").addEventListener('click',startChangingColor);
+
+document.querySelector("#stop").addEventListener('click',stopChangingColor);
